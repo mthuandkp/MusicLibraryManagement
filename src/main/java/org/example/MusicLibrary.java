cfa57ef0d12990d7443e4b39bb454e52f3111224
mthuandkp <pnmthuan@gmail.com>
2023-07-01 23:25:38 +0700

    add gitignore

--- src/main/java/org/example/MusicLibrary.java
+++ src/main/java/org/example/MusicLibrary.java
@@ -6,8 +6,8 @@ import org.example.Gui.UI;
 public class MusicLibrary {
     public static void main(String[] args) {
         UI cliUI = new UI(null);
-        //while (true) {
-            cliUI.login();
-        //}
+
+        cliUI.login();
+
     }
 }
